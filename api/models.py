ddd4e5044c8f088db39e6719fdddc2b55dcc00eb
shoxruxmirzoo <48640008+shoxruxmirzoo@users.noreply.github.com>
2020-05-02 22:34:38 +0500

    Add files via upload

---
+++ api/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+
+# Create your models here.
+class Book(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class LibUser(models.Model):
+    name = models.CharField(max_length=100)
+    def __str__(self):
+        return self.name
+
+
+class RentBook(models.Model):
+    book = models.ForeignKey(Book, on_delete = models.CASCADE)
+    user = models.ForeignKey(LibUser, on_delete = models.CASCADE)
+    rentDate = models.DateTimeField(auto_now_add = True )
+    returnedDate = models.DateTimeField(null = True, blank = True )
+    def __str__(self):
+        return self.rentDate
